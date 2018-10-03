e00ec27b221a3caa8ebd850c170d3abba6315347
Eros <george.marr.cobc@gmail.com>
2018-10-03 14:11:56 +0100

    fuck

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
 extern crate mio;
 use mio::*;
-use std::net::SocketAddr;
 use mio::tcp::*;
+use std::net::SocketAddr;
+use std::collections::HashMap;
 
-let address = "0.0.0.0:0000".parse::<SocketAddr>().unwrap();
-let server_socket = TcpListener::bind(&address).unwrap();
+static const SERVER_TOKEN: Token = Token(0);
 
-struct WebSocketServer;
+struct WebSocketServer{
+    socket: TcpListener,
+    clients: HashMap<Token, TcpStream>,
+    token_counter: usize
+}
 
 impl Handler for WebSocketServer {
     type Timeout = usize;
     type Message = ();
+
+    fn ready(&mut self, event_loop: &mut EventLoop<WebSocketServer>, token: Token, events: EventSet){
+        match token {
+            SERVER_TOKEN =>
+
+        }
+    }
 }
 
 fn main() {
+
+    let mut address = "0.0.0.0:0000".parse::<SocketAddr>().unwrap();
+    let server_socket = TcpListener::bind(&address).unwrap();
+
     let mut event_loop = EventLoop::new().unwrap();
     let mut handler = WebSocketServer;
     event_loop.run(&mut handler).unwrap();
